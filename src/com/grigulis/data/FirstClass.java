6baf7d30dbd5ded116d3eeb7c0ca350a72d18f1c
Sandis Grigulis <sandis.grigulis@gmail.com>
2018-11-25 10:05:11 +0200

    My first commit in first branch

--- src/com/grigulis/data/FirstClass.java
+++ src/com/grigulis/data/FirstClass.java
@@ -3,4 +3,15 @@ package com.grigulis.data;
 public class FirstClass {
     private String field;
 
+    public FirstClass(String field) {
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
 }
